288a747db00bcbd6a10f69c086a7386c3274ea3d
Chetan Yadav <ydvchetan01@gmail.com>
2025-08-21 18:20:19 +0530

    auth page

--- frontend/src/pages/Auth.tsx
+++ frontend/src/pages/Auth.tsx
@@ -1,7 +1,41 @@
-const Auth = () => {
-  return (
-    <div>Auth</div>
-  )
-}
+import { useState } from "react";
+import Signup from "@/components/auth/Signup";
+import Signin from "@/components/auth/Signin";
+import HeaderTwo from "@/components/HeaderTwo";
 
-export default Auth
+const Auth = () => {
+  const [isSignIn, setIsSignIn] = useState(true);
+  return (
+    <>
+      <HeaderTwo />
+      <div className="flex flex-row justify-center mt-10">
+        <div className="w-1/3 border rounded-2xl p-6 flex flex-col gap-7">
+          <h2 className="text-4xl font-bold text-blue-700 text-center">
+            Vidora
+          </h2>
+          <div className="bg-gray-100 rounded-lg p-1 flex flex-row justify-center items-center text-center font-semibold">
+            <div
+              className={`${isSignIn ? "bg-white" : "text-gray-500"} w-1/2 cursor-pointer rounded-lg p-1`}
+              onClick={() => {
+                setIsSignIn(true);
+              }}
+            >
+              Sign In
+            </div>
+            <div
+              className={`${isSignIn ? "text-gray-500" : "bg-white"} w-1/2 cursor-pointer rounded-lg p-1`}
+              onClick={() => {
+                setIsSignIn(false);
+              }}
+            >
+              Sign Up
+            </div>
+          </div>
+          {isSignIn ? <Signin /> : <Signup />}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Auth;
