d11ceb586a86a1a3510ffd906b40e17f320c130a
Chetan Yadav <ydvchetan01@gmail.com>
2025-10-04 17:47:45 +0530

    refresh and access token methods

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -48,8 +48,26 @@ const userSchema = new Schema(
   { timestamps: true }
 );
 
-export const User = mongoose.model("User", userSchema);
-
-userSchema.methods.isPasswordCorrect = function () {
-  this.password 
+userSchema.methods.generateAccesstoken = function () {
+  return jwt.sign(
+    { id: this._id, email: this.email },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
 };
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export const User = mongoose.model("User", userSchema);
