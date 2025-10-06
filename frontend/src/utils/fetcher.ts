6e75f765120cfe60753aaf2dd29bb1df1af9302f
Chetan Yadav <ydvchetan01@gmail.com>
2025-10-06 21:13:45 +0530

    access token refresh

--- frontend/src/utils/fetcher.ts
+++ frontend/src/utils/fetcher.ts
@@ -4,12 +4,9 @@ export const authFetch = async (
   url: RequestInfo | URL,
   init: RequestInit = {}
 ): Promise<Response> => {
-  const { accessToken } = userStore.getState();
+  const { accessToken, setAccessToken } = userStore.getState();
 
-  if (!accessToken) {
-    console.error("Acess Token missing. User must login");
-    throw new Error("Unauthroized: Access Token Missing");
-  }
+  if (!accessToken) throw new Error("Unauthroized: Access Token Missing");
 
   const headers = {
     ...init.headers,
@@ -22,5 +19,37 @@ export const authFetch = async (
     headers,
   };
 
-  return fetch(url, config);
+  let response = await fetch(url, config);
+
+  if (response.status === 401) {
+    try {
+      const refreshRes = await fetch(
+        "http://localhost:3000/api/users/refresh-token",
+        {
+          method: "POST",
+          credentials: "include",
+        }
+      );
+
+      if (!refreshRes.ok) {
+        throw new Error("Refresh token failed");
+      }
+
+      const data = await refreshRes.json();
+      setAccessToken(data.accessToken);
+
+      const retryHeaders = {
+        ...init.headers,
+        Authorization: `Bearer ${data.accessToken}`,
+        "Content-Type": "application/json",
+      };
+
+      const retryConfig: RequestInit = { ...init, headers: retryHeaders };
+      response = await fetch(url, retryConfig);
+    } catch (err) {
+      console.error("Token refresh failed:", err);
+      throw err;
+    }
+  }
+  return response;
 };
